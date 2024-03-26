9b41b7681599a4d3715fa1aa1fb32467344f04be
Gazi Akter <gaziakterdev@gmail.com>
2024-03-26 15:00:57 +0600

    resister footer sidebar

--- functions.php
+++ functions.php
@@ -45,16 +45,61 @@ function persona_support(){
         'side-menu'     =>  __('Side Menu', 'persona')
     ) );
 
+    /** Remove blog editior */
+    remove_theme_support( 'widgets-block-editor' );
+
 }
 
 add_action( 'after_setup_theme', 'persona_support' );
 
 
+/** Resister sidebar */
+function persona_widgets() {
+	register_sidebar( array(
+		'name'          => __( 'Footer Widget 01', 'persona' ),
+		'id'            => 'footer-widget-01',
+		'description'   => __( 'Widgets in this area will be shown on footer.', 'persona' ),
+		'before_widget' => '<div id="%1$s" class="footer__widget mb-50 footer-col-1 %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="footer__widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+    register_sidebar( array(
+		'name'          => __( 'Footer Widget 02', 'persona' ),
+		'id'            => 'footer-widget-02',
+		'description'   => __( 'Widgets in this area will be shown on footer.', 'persona' ),
+		'before_widget' => '<div id="%1$s" class="footer__widget mb-50 footer-col-2 %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="footer__widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+    register_sidebar( array(
+		'name'          => __( 'Footer Widget 03', 'persona' ),
+		'id'            => 'footer-widget-03',
+		'description'   => __( 'Widgets in this area will be shown on footer.', 'persona' ),
+		'before_widget' => '<div id="%1$s" class="footer__widget mb-50 footer-col-3 %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="footer__widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+    register_sidebar( array(
+		'name'          => __( 'Footer Widget 04', 'persona' ),
+		'id'            => 'footer-widget-04',
+		'description'   => __( 'Widgets in this area will be shown on footer.', 'persona' ),
+		'before_widget' => '<div id="%1$s" class="footer__widget mb-50 footer-col-4 %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="footer__widget-title">',
+		'after_title'   => '</h3>',
+	) );
+}
+add_action( 'widgets_init', 'persona_widgets' );
 
 
 
-
-/** Include file */
+/** Include files */
 include_once('inc/common/scripts.php');
 include_once('inc/template-function.php');
 include_once('inc/nav-walker.php');
