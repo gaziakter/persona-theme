0c6285f02f2e7a9e415dbf98864b9e4b6ac1a885
Gazi Akter <gaziakterdev@gmail.com>
2024-04-21 07:44:49 +0600

    single post audio format

--- template-parts/content-audio.php
+++ template-parts/content-audio.php
@@ -1,7 +1,36 @@
 
 <?php $audio_url = function_exists('get_field') ? get_field('insert_audio_url'): null; ?>
 
+<?php if(is_single( )) : ?>
+<article id="post-<?php the_ID(); ?>" class="<?php post_class( 'persona-formate-audio persona-single-page' ); ?>">
 
+<?php if(!empty($audio_url)): ?>
+    <div class="postbox__thumb postbox__audio w-img p-relative">
+       <?php echo wp_oembed_get($audio_url); ?>
+    </div>
+    <?php endif; ?>
+    <!-- Start post meta -->>
+    <?php echo get_template_part( 'template-parts/blog/post-meta' ); ?>
+
+    <div class="postbox__details-content-wrapper mb-40">
+        <?php the_content(); ?>
+    </div>
+
+    <div class="postbox__share-wrapper mb-60">
+        <div class="row align-items-center">
+            <div class="col-xl-7">
+                <div class="tagcloud tagcloud-sm">
+                    <span><?php echo esc_html__('Tags:', 'persona'  ); ?></span>
+                    <?php persona_tags(); ?>
+                </div>
+            </div>
+            <div class="col-xl-5">
+                <?php persona_social_share(); ?>
+            </div>
+        </div>
+    </div>
+</article>
+<?php else : ?>
 
 <article id="post-<?php the_ID(); ?>"
     class="<?php post_class( 'persona-formate-audio postbox__item format-audio mb-50 transition-3' ); ?>">
@@ -24,3 +53,5 @@
         </div>
     </div>
 </article>
+
+<?php endif; ?>
