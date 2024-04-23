6047ac88158cfedc0dbf078a744a935417f7f442
Gazi Akter <gaziakterdev@gmail.com>
2024-04-23 07:06:23 +0600

    404 not found

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
             <div class="postbox__wrapper">
                <?php if (have_posts()) : ?>
                   <?php while (have_posts()) : the_post() ?>
-                     <?php echo get_template_part( 'template-parts/content', get_post_format(  ) ) ?>
+                     <?php echo get_template_part( 'template-parts/content', get_post_format(  ) ); ?>
                   <?php endwhile; ?>
+                  <?php else: ?>
+                     <?php echo get_template_part( 'template-parts/content-none'); ?>
                <?php endif; ?>
 
                <div class="tp-pagination tp-pagination-style-2 mt-20">
---
+++ template-parts/content-none.php
@@ -0,0 +1,1 @@
+<p>Content not found</p>
