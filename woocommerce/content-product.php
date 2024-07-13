e41fbb37445460d8b986ccb6fb8857e06302b4dc
Gazi Akter <gaziakterdev@gmail.com>
2024-07-13 11:46:54 +0600

    insert the html

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -26,6 +26,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 ?>
 <div <?php wc_product_class( '', $product ); ?>>
 	<?php
+	echo "helo";
 	/**
 	 * Hook: woocommerce_before_shop_loop_item.
 	 *
