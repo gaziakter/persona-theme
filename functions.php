c3f82e303035be2ed86a66a5b87c2fb4c7591b70
Gazi Akter <gaziakterdev@gmail.com>
2024-07-16 06:17:37 +0600

    wc-product-gallery-lightbox

--- functions.php
+++ functions.php
@@ -3,13 +3,14 @@
 /**
  * Essential theme supports
  * */
-function persona_support() {
+function persona_support()
+{
 
     /** automatic feed link*/
-    add_theme_support( 'automatic-feed-links' );
+    add_theme_support('automatic-feed-links');
 
     /** tag-title **/
-    add_theme_support( 'title-tag' );
+    add_theme_support('title-tag');
 
     /** post formats */
     $post_formats = [
@@ -22,120 +23,133 @@ function persona_support() {
         'quote',
         'status',
     ];
-    add_theme_support( 'post-formats', $post_formats );
+    add_theme_support('post-formats', $post_formats);
 
     /** post thumbnail **/
-    add_theme_support( 'post-thumbnails' );
+    add_theme_support('post-thumbnails');
 
     /** WooCommerce support */
-    add_theme_support( 'woocommerce' );
+    add_theme_support('woocommerce');
+    add_theme_support('wc-product-gallery-zoom');
+    add_theme_support('wc-product-gallery-lightbox');
+    add_theme_support('wc-product-gallery-slider');
 
     /** refresh widgest **/
-    add_theme_support( 'customize-selective-refresh-widgets' );
+    add_theme_support('customize-selective-refresh-widgets');
 
     /** HTML5 support **/
-    add_theme_support( 'html5', [
+    add_theme_support('html5', [
         'comment-list',
         'comment-form',
         'search-form',
         'gallery',
         'caption',
-    ] );
+    ]);
 
     /** Register Menus */
-    register_nav_menus( [
-        'main-menu'   => __( 'Main Menu', 'persona' ),
-        'side-menu'   => __( 'Side Menu', 'persona' ),
-        'footer-menu' => __( 'Footer Menu', 'persona' ),
-    ] );
+    register_nav_menus([
+        'main-menu'   => __('Main Menu', 'persona'),
+        'side-menu'   => __('Side Menu', 'persona'),
+        'footer-menu' => __('Footer Menu', 'persona'),
+    ]);
 
     /** Remove blog editior */
-    remove_theme_support( 'widgets-block-editor' );
+    remove_theme_support('widgets-block-editor');
 
     /** Remove woocommerce default style */
-    add_filter( 'woocommerce_enqueue_styles', '__return_false');
-
+    add_filter('woocommerce_enqueue_styles', '__return_false');
 }
 
-add_action( 'after_setup_theme', 'persona_support' );
+add_action('after_setup_theme', 'persona_support');
 
 /** Resister sidebar */
-function persona_widgets() {
+function persona_widgets()
+{
 
     /** Blog sidebar */
-    register_sidebar( [
-        'name'          => __( 'Blog Sidebar', 'persona' ),
+    register_sidebar([
+        'name'          => __('Blog Sidebar', 'persona'),
         'id'            => 'blog-sidebar',
         'description'   =>
-        __( 'Widgets in this area will be shown on blog sidebar.', 'persona' ),
+        __('Widgets in this area will be shown on blog sidebar.', 'persona'),
         'before_widget' =>
         '<div id="%1$s" class="sidebar__widget mb-40 %2$s">',
         'after_widget'  => '</div>',
         'before_title'  => '<h3 class="sidebar__widget-title">',
         'after_title'   => '</h3>',
-    ] );
+    ]);
 
-    register_sidebar( [
-        'name'          => __( 'Footer Widget 01', 'persona' ),
+    register_sidebar([
+        'name'          => __('Footer Widget 01', 'persona'),
         'id'            => 'footer-widget-01',
-        'description'   => __( 'Widgets in this area will be shown on footer.',
-            'persona' ),
+        'description'   => __(
+            'Widgets in this area will be shown on footer.',
+            'persona'
+        ),
         'before_widget' =>
         '<div id="%1$s" class="footer__widget mb-50 footer-col-1 %2$s">',
         'after_widget'  => '</div>',
         'before_title'  => '<h3 class="footer__widget-title">',
         'after_title'   => '</h3>',
-    ] );
+    ]);
 
-    register_sidebar( [
-        'name'          => __( 'Footer Widget 02', 'persona' ),
+    register_sidebar([
+        'name'          => __('Footer Widget 02', 'persona'),
         'id'            => 'footer-widget-02',
-        'description'   => __( 'Widgets in this area will be shown on footer.',
-            'persona' ),
+        'description'   => __(
+            'Widgets in this area will be shown on footer.',
+            'persona'
+        ),
         'before_widget' =>
         '<div id="%1$s" class="footer__widget mb-50 footer-col-2 %2$s">',
         'after_widget'  => '</div>',
         'before_title'  => '<h3 class="footer__widget-title">',
         'after_title'   => '</h3>',
-    ] );
+    ]);
 
-    register_sidebar( [
-        'name'          => __( 'Footer Widget 03', 'persona' ),
+    register_sidebar([
+        'name'          => __('Footer Widget 03', 'persona'),
         'id'            => 'footer-widget-03',
-        'description'   => __( 'Widgets in this area will be shown on footer.',
-            'persona' ),
+        'description'   => __(
+            'Widgets in this area will be shown on footer.',
+            'persona'
+        ),
         'before_widget' =>
         '<div id="%1$s" class="footer__widget mb-50 footer-col-3 %2$s">',
         'after_widget'  => '</div>',
         'before_title'  => '<h3 class="footer__widget-title">',
         'after_title'   => '</h3>',
-    ] );
+    ]);
 
-    register_sidebar( [
-        'name'          => __( 'Footer Widget 04', 'persona' ),
+    register_sidebar([
+        'name'          => __('Footer Widget 04', 'persona'),
         'id'            => 'footer-widget-04',
-        'description'   => __( 'Widgets in this area will be shown on footer.',
-            'persona' ),
+        'description'   => __(
+            'Widgets in this area will be shown on footer.',
+            'persona'
+        ),
         'before_widget' =>
         '<div id="%1$s" class="footer__widget mb-50 footer-col-4 %2$s">',
         'after_widget'  => '</div>',
         'before_title'  => '<h3 class="footer__widget-title">',
         'after_title'   => '</h3>',
-    ] );
+    ]);
 
-    register_sidebar( [
-        'name'          => __( 'Shop Sidebar', 'persona' ),
+    register_sidebar([
+        'name'          => __('Shop Sidebar', 'persona'),
         'id'            => 'shop-sidebar',
-        'description'   => __( 'Widgets in this area will be shown on shop page sidebar.',
-            'persona' ),
+        'description'   => __(
+            'Widgets in this area will be shown on shop page sidebar.',
+            'persona'
+        ),
         'before_widget' =>
         '<div id="%1$s" class="shop__widget tp-accordion %2$s">',
         'after_widget'  => '</div>',
         'before_title'  => '<h3 class="footer__widget-title">',
         'after_title'   => '</h3>',
-    ] );
+    ]);
 }
-add_action( 'widgets_init', 'persona_widgets' );
+add_action('widgets_init', 'persona_widgets');
 
 /** Include files */
 include_once 'inc/common/scripts.php';
@@ -145,11 +159,11 @@ include_once 'inc/sidebar-rc-post-widget.php';
 include_once 'inc/breadcorumb.php';
 
 /** Include kirki file */
-if ( class_exists( 'kirki' ) ) {
+if (class_exists('kirki')) {
     include_once 'inc/persona-kirki.php';
 }
 
 /** Woocommerce file inclode */
-if ( class_exists( 'WooCommerce' ) ) {
+if (class_exists('WooCommerce')) {
     include_once 'inc/woo.php';
 }
