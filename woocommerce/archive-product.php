0aea481ba20dc9760d99b39c28c6b1252d0136b3
Gazi Akter <gaziakterdev@gmail.com>
2024-08-28 07:14:27 +0600

    content product list

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -29,52 +29,52 @@ get_header('shop');
  */
 do_action('woocommerce_before_main_content');
 ?>
-               <div class="shop__top mb-50">
-                  <div class="row align-items-center">
-                     <div class="col-lg-6 col-md-5">
-                        <div class="shop__result">
-                           <?php woocommerce_result_count(); ?>
-                        </div>
-                     </div>
-                     <div class="col-lg-6 col-md-7">
-                        <div class="shop__sort d-flex flex-wrap justify-content-md-end align-items-center">
-                           <div class="shop__sort-item">
-                              <div class="shop__sort-tab tp-tab">
-                                 <nav>
-                                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                                      <button class="nav-link active" id="nav-grid-tab" data-bs-toggle="tab" data-bs-target="#nav-grid" type="button" role="tab" aria-controls="nav-grid" aria-selected="true">
-                                       <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                          <path d="M19.65 6.868V2.782C19.65 1.513 19.074 1 17.643 1H14.007C12.576 1 12 1.513 12 2.782V6.859C12 8.137 12.576 8.641 14.007 8.641H17.643C19.074 8.65 19.65 8.137 19.65 6.868Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                          <path d="M19.65 17.643V14.007C19.65 12.576 19.074 12 17.643 12H14.007C12.576 12 12 12.576 12 14.007V17.643C12 19.074 12.576 19.65 14.007 19.65H17.643C19.074 19.65 19.65 19.074 19.65 17.643Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                          <path d="M8.65 6.868V2.782C8.65 1.513 8.074 1 6.643 1H3.007C1.576 1 1 1.513 1 2.782V6.859C1 8.137 1.576 8.641 3.007 8.641H6.643C8.074 8.65 8.65 8.137 8.65 6.868Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                          <path d="M8.65 17.643V14.007C8.65 12.576 8.074 12 6.643 12H3.007C1.576 12 1 12.576 1 14.007V17.643C1 19.074 1.576 19.65 3.007 19.65H6.643C8.074 19.65 8.65 19.074 8.65 17.643Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                       </svg>                                          
-                                      </button>
-                                      <button class="nav-link" id="nav-list-tab" data-bs-toggle="tab" data-bs-target="#nav-list" type="button" role="tab" aria-controls="nav-list" aria-selected="false">
-                                          <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                             <path d="M1 1H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                             <path d="M1 8H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                             <path d="M1 15H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                          </svg>                                          
-                                      </button>
-                                    </div>
-                                  </nav>                                  
-                              </div>
-                           </div>
-                           <div class="shop__sort-item">
-                              <div class="shop__sort-select">
-                                 <select>
-                                    <option>Sort by latest</option>
-                                    <option>Sort by best selling</option>
-                                    <option>Sort by top week</option>
-                                 </select>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-<?php 
+<div class="shop__top mb-50">
+	<div class="row align-items-center">
+		<div class="col-lg-6 col-md-5">
+			<div class="shop__result">
+				<?php woocommerce_result_count(); ?>
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-7">
+			<div class="shop__sort d-flex flex-wrap justify-content-md-end align-items-center">
+				<div class="shop__sort-item">
+					<div class="shop__sort-tab tp-tab">
+						<nav>
+							<div class="nav nav-tabs" id="nav-tab" role="tablist">
+								<button class="nav-link active" id="nav-grid-tab" data-bs-toggle="tab" data-bs-target="#nav-grid" type="button" role="tab" aria-controls="nav-grid" aria-selected="true">
+									<svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M19.65 6.868V2.782C19.65 1.513 19.074 1 17.643 1H14.007C12.576 1 12 1.513 12 2.782V6.859C12 8.137 12.576 8.641 14.007 8.641H17.643C19.074 8.65 19.65 8.137 19.65 6.868Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+										<path d="M19.65 17.643V14.007C19.65 12.576 19.074 12 17.643 12H14.007C12.576 12 12 12.576 12 14.007V17.643C12 19.074 12.576 19.65 14.007 19.65H17.643C19.074 19.65 19.65 19.074 19.65 17.643Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+										<path d="M8.65 6.868V2.782C8.65 1.513 8.074 1 6.643 1H3.007C1.576 1 1 1.513 1 2.782V6.859C1 8.137 1.576 8.641 3.007 8.641H6.643C8.074 8.65 8.65 8.137 8.65 6.868Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+										<path d="M8.65 17.643V14.007C8.65 12.576 8.074 12 6.643 12H3.007C1.576 12 1 12.576 1 14.007V17.643C1 19.074 1.576 19.65 3.007 19.65H6.643C8.074 19.65 8.65 19.074 8.65 17.643Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+									</svg>
+								</button>
+								<button class="nav-link" id="nav-list-tab" data-bs-toggle="tab" data-bs-target="#nav-list" type="button" role="tab" aria-controls="nav-list" aria-selected="false">
+									<svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M1 1H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+										<path d="M1 8H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+										<path d="M1 15H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+									</svg>
+								</button>
+							</div>
+						</nav>
+					</div>
+				</div>
+				<div class="shop__sort-item">
+					<div class="shop__sort-select">
+						<select>
+							<option>Sort by latest</option>
+							<option>Sort by best selling</option>
+							<option>Sort by top week</option>
+						</select>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
 
 
 /**
@@ -86,20 +86,20 @@ do_action('woocommerce_before_main_content');
  */
 do_action('woocommerce_shop_loop_header');
 
-$product_col = (is_active_sidebar('shop-sidebar')) ? '9': '12';
+$product_col = (is_active_sidebar('shop-sidebar')) ? '9' : '12';
 
 if (woocommerce_product_loop()) {
 ?>
 	<div class="shop__main">
 		<div class="row">
-			<?php if(is_active_sidebar('shop-sidebar')): ?>
-			<div class="col-lg-3">
-				<!-- if sidebar on left add class on-left -->
-				<!-- if sidebar on right add class on-right -->
-				<div class="shop__sidebar on-left">
-					<?php dynamic_sidebar('shop-sidebar'); ?>
+			<?php if (is_active_sidebar('shop-sidebar')): ?>
+				<div class="col-lg-3">
+					<!-- if sidebar on left add class on-left -->
+					<!-- if sidebar on right add class on-right -->
+					<div class="shop__sidebar on-left">
+						<?php dynamic_sidebar('shop-sidebar'); ?>
+					</div>
 				</div>
-			</div>
 			<?php endif; ?>
 			<div class="col-lg-<?php echo esc_attr($product_col); ?> order-first order-lg-last">
 				<div class="shop__tab-content mb-40">
@@ -149,7 +149,42 @@ if (woocommerce_product_loop()) {
 							<!-- shop list -->
 							<div class="product__list-wrapper mb-30">
 								<div class="row">
+									<?php
 
+									/**
+									 * Hook: woocommerce_before_shop_loop.
+									 *
+									 * @hooked woocommerce_output_all_notices - 10
+									 * @hooked woocommerce_result_count - 20
+									 * @hooked woocommerce_catalog_ordering - 30
+									 */
+									do_action('woocommerce_before_shop_loop');
+
+									woocommerce_product_loop_start();
+
+									if (wc_get_loop_prop('total')) {
+										while (have_posts()) {
+											the_post();
+
+											/**
+											 * Hook: woocommerce_shop_loop.
+											 */
+											do_action('woocommerce_shop_loop');
+
+											wc_get_template_part('content', 'product-list');
+										}
+									}
+
+									woocommerce_product_loop_end();
+
+									/**
+									 * Hook: woocommerce_after_shop_loop.
+									 *
+									 * @hooked woocommerce_pagination - 10
+									 */
+									do_action('woocommerce_after_shop_loop');
+
+									?>
 								</div>
 							</div>
 						</div>
